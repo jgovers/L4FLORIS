ed5d5397ca96a061d1f3ba07a1e5e44422b0a0a3
Bartdoekemeijer <B.M.Doekemeijer@tudelft.nl>
2017-04-09 12:25:31 +0200

    Clean up code here and there. Fix error for saving LUT.

--- LUT_generation/setupInflowFiles.m
+++ LUT_generation/setupInflowFiles.m
@@ -2,7 +2,7 @@ clear all; close all; clc;
 addpath bin % Add binary files (external functions)
 
 % LUT settings: overwrite default parameters
-inputData.parameters       = {'yWake'} %{'yWake','u_mean'};    % Variable names to cover
+inputData.parameters       = {'yWake'}; %{'yWake','u_mean'};    % Variable names to cover
 inputData.ranges           = {-20:20:20} %{-20:20:20,6:1:8};     % Ranges to cover for LUT.parameters
 
 % Set up settings
--- main.m
+++ main.m
@@ -27,7 +27,7 @@ siteStruct.rho      = 1.1716;   % Atmospheric air density (kg/m3)
 
 % Setup optimization settings
 optimStruct.optConst        = 1;                            % Weighting factor. Power only = 1, Loads only = 0.
-optimStruct.iterations      = 500;                          % Optimization iterations  [-]
+optimStruct.iterations      = 100;                          % Optimization iterations  [-]
 optimStruct.maxYaw          = +30;                          % Largest  yaw angle [radians]
 optimStruct.minYaw          = -30;                          % Smallest yaw angle [radians]
 optimStruct.axInd           = 1/3*ones(size(siteStruct.LocIF,1)); % Axial induction factors
